bc5e244e1504462665f57c90052d51fe19e0e931
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-06-02 09:06:41 +0200

    userlogin

---
+++ src/main/java/com/petvacation/petvacation/service/UserServiceImpl.java
@@ -0,0 +1,52 @@
+package com.petvacation.petvacation.service;
+
+import com.petvacation.petvacation.domain.Role;
+import com.petvacation.petvacation.domain.User;
+import com.petvacation.petvacation.repository.RoleRepository;
+import com.petvacation.petvacation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service @RequiredArgsConstructor @Transactional @Slf4j
+public class UserServiceImpl implements IUserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+
+    @Override
+    public User saveUser(User user) {
+        log.info("Saving new user {} to the database", user.getName());
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database", role.getName());
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {} ", roleName, username);
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        log.info("Fetching user {}",  username);
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        log.info("Fetching all users");
+        return userRepository.findAll();
+    }
+}
