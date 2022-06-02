d382359945192801ec5bd1500fd3c0c782904fb6
Lupe13 <lupe.flores.r@gmail.com>
2022-06-02 09:13:28 +0200

    owner

--- src/main/java/com/petvacation/petvacation/domain/OwnerEntity.java
+++ src/main/java/com/petvacation/petvacation/domain/OwnerEntity.java
@@ -1,4 +1,19 @@
 package com.petvacation.petvacation.domain;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "owner")
 public class OwnerEntity {
+
+    private static final long serialVersionUId = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+
+
+
 }
