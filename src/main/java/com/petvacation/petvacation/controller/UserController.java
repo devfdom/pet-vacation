857bdd24445520ea5567630a0179ff833dfbd081
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-06-03 08:51:38 +0200

    segond step login

--- src/main/java/com/petvacation/petvacation/controller/UserController.java
+++ src/main/java/com/petvacation/petvacation/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.petvacation.petvacation.controller;
 
+import com.petvacation.petvacation.domain.Role;
 import com.petvacation.petvacation.domain.User;
 import com.petvacation.petvacation.service.IUserService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -17,10 +19,35 @@ public class UserController {
     private final IUserService iUserService;
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getUsers(){
+    public ResponseEntity<List<User>>getUsers(){
         return ResponseEntity.ok().body(iUserService.getUsers());
+    }
 
+    @PostMapping("/user/save")
+    public ResponseEntity<User>saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(iUserService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(iUserService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form){
+        iUserService.addRoleToUser(form.getUsername(),form.getRoleName());
+        return ResponseEntity.ok().build();
     }
 
 
 }
+
+    @Data
+    class RoleToUserForm{
+        private String username;
+        private String roleName;
+
+    }
+
--- src/main/java/com/petvacation/petvacation/service/UserServiceImpl.java
+++ src/main/java/com/petvacation/petvacation/service/UserServiceImpl.java
@@ -6,17 +6,38 @@ import com.petvacation.petvacation.repository.RoleRepository;
 import com.petvacation.petvacation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements IUserService {
+public class UserServiceImpl implements IUserService, UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else{
+            log.info("User found in the database: {}",username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassworld(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -49,4 +70,6 @@ public class UserServiceImpl implements IUserService {
         log.info("Fetching all users");
         return userRepository.findAll();
     }
+
+
 }
